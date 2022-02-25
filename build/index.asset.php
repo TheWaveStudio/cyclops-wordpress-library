a2fbaaf5cfd11c0328fceefe3840b31d7317c693
Luca Pagliaro <pagliaroluca@gmail.com>
2022-02-25 14:03:27 +0100

    build:

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '70652307b744d46efd712152ed1b3d09');
-=======
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'ee3cdb02914bbc4c10247a0372868973');
->>>>>>> 7a709438eb4072f0e8112ad503917806486fdb9f
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '6827cc566765abf1b2d6d1cd81534734');
