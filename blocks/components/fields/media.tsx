463bf7a630709621c694af1cf8d0007e863e5d71
Francesca Patanè <francesca.patane@thewavestudio.it>
2022-05-04 10:32:07 +0200

    fix: image block label

--- blocks/components/fields/media.tsx
+++ blocks/components/fields/media.tsx
@@ -6,28 +6,28 @@ export const MediaField: FC<MediaFieldProps> = props => {
 
   const {label = "", onChange, value, placeholder} = props;
   const {value: {id: currentSelectedId}} = props;
-  const isMediaSeleced = useMemo(() => !!currentSelectedId, [currentSelectedId]);
+  const isMediaSelected = useMemo(() => !!currentSelectedId, [currentSelectedId]);
 
   return <div>
-    {!isMediaSeleced &&
-    <MediaPlaceholder onSelect={onChange}
-                      allowedTypes={['image']}
-                      multiple={false}
-                      labels={{title: 'The Image'}}
-                      value={props.value}
-                      addToGallery={true}/>}
-    {isMediaSeleced &&
-    <div>
-      <div className="wf-media-preview">
-        <img src={props.value.url} alt={props.value.alt}/>
-      </div>
+    {!isMediaSelected &&
+      <MediaPlaceholder onSelect={onChange}
+                        allowedTypes={['image']}
+                        multiple={false}
+                        labels={{title: label ?? 'The Image'}}
+                        value={props.value}
+                        addToGallery={true}/>}
+    {isMediaSelected &&
       <div>
-        <button type="button"
-                onClick={e => props.onChange({})}
-        >Rimuovi Immagine
-        </button>
-      </div>
-    </div>}
+        <div className="wf-media-preview">
+          <img src={props.value.url} alt={props.value.alt}/>
+        </div>
+        <div>
+          <button type="button"
+                  onClick={e => props.onChange({})}
+          >Rimuovi Immagine
+          </button>
+        </div>
+      </div>}
   </div>
 }
 
